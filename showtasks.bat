dfc66044b66b1df57e472e70ab62d6fbd9705831
justynaWoj <j.wojciechowska2011@gmail.com>
2020-09-26 20:17:29 +0200

    21.4

---
+++ showtasks.bat
@@ -0,0 +1,26 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo cannot run runcrud
+goto fail
+
+:runbrowser
+
+start chrome
+if "%ERRORLEVEL%" == "0" goto setwebsite
+echo Cannot open browser
+goto fail
+
+:setwebsite
+start "" http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo Cannot load http://localhost:8080/crud/v1/task/getTasks
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Tasks have been shown.
