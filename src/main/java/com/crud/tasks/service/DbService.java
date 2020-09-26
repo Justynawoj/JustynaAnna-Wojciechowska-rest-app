21f045c641aa9933930cee9f0665e306df826c36
justynaWoj <j.wojciechowska2011@gmail.com>
2020-09-26 20:01:19 +0200

    19.2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class DbService {
@@ -19,9 +19,8 @@ public class DbService {
     }
 
 
-    public Optional<Task> findTaskById(long id){
-        return repository.findById(id);
-        //return new Task();
+    public Task findTaskById(long id){
+        return repository.findById(id).orElseThrow(()->new RuntimeException("Not found"));
     }
 
 
