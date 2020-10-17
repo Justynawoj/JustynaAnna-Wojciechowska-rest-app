4108d4b1a1aaffe226e0ce46b502cd0b13419426
justynaWoj <j.wojciechowska2011@gmail.com>
2020-10-17 17:03:45 +0200

    23.3 Exercise

--- src/main/java/com/crud/tasks/domains/Mail.java
+++ src/main/java/com/crud/tasks/domains/Mail.java
@@ -7,6 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Mail {
     private String mailTo;
+    private String toCc;
     private String subject;
     private String message;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
 }
--- src/test/java/com/crud/tasks/services/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/services/SimpleEmailServiceTest.java
@@ -26,11 +26,12 @@ public class SimpleEmailServiceTest {
     public void shouldSendEmail() {
 
         //Given
-
         Mail mail = new Mail("test@test.com", "Test", "Test message");
-
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
+        if(mail.getToCc()!=null) {
+            mailMessage.setCc(mail.getToCc());
+        }
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
 
@@ -39,6 +40,5 @@ public class SimpleEmailServiceTest {
 
         //Then
         verify(javaMailSender,times(1)).send(mailMessage);
-
     }
 }
